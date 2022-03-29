fb7662a08174f56e4d2dcb7f6d4d25d6570242ce
Oliver Sabler <oliver.sabler@gmail.com>
2022-03-29 19:58:21 +0200

    Plot mask data

--- diagram.py
+++ diagram.py
@@ -1,6 +1,6 @@
-from xml.sax.saxutils import XMLFilterBase
 import xmltodict
-from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt 
 from os import listdir
 
 ANNOTATIONS_PATH = './data/annotations/'
@@ -11,7 +11,7 @@ for filename in listdir(ANNOTATIONS_PATH):
   ann_file_path.append(ANNOTATIONS_PATH + filename)
 
 # Loop all annotation files and save mask data to list
-mask_data = []
+data = []
 for file in ann_file_path:
   with open(file) as f:
     # Parse to dict and select the 'object' element(s) 
@@ -21,11 +21,15 @@ for file in ann_file_path:
     # If the object is a list loop through it, else select name
     if type(obj) == list:
       for o in obj:
-        mask_data.append(o['name'])
+        data.append(o['name'])
     else:
-      mask_data.append(obj['name'])
+      data.append(obj['name'])
 
-# Todo: plot mask_data
-print(mask_data.count('with_mask'))
-print(mask_data.count('without_mask'))
-print(mask_data.count('mask_weared_incorrect'))
+# Plot mask data
+x = ['With Mask', 'Without Mask', 'Mask Weared Incorrect']
+y = [data.count('with_mask'), data.count('without_mask'), data.count('mask_weared_incorrect')]
+
+fig, ax = plt.subplots()
+ax.bar(x, y)
+
+plt.savefig('mask_data.png')
