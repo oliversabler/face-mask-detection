8d2c8a3191576948e1eb321cb85cff273fad2a7d
Oliver Sabler <oliver.sabler@gmail.com>
2022-04-02 10:48:48 +0200

    More refac, prepare image loading

--- diagram.py
+++ diagram.py
@@ -3,35 +3,46 @@ import numpy as np
 from matplotlib import pyplot as plt 
 from os import listdir
 
-METADATA_PATH = './data/annotations/'
+def get_paths(path):
+  paths = []
+  for filename in listdir(path):
+    paths.append(path + filename)
+  return paths
 
-# Get a paths for all annotation files
 def get_metadata_paths():
-  metadata_paths = []
-  for filename in listdir(METADATA_PATH):
-    metadata_paths.append(METADATA_PATH + filename)
-  return metadata_paths
+  return get_paths('./data/annotations/')
+
+def get_images_paths():
+  return get_paths('./data/images/')
 
 # Loop all annotation files and save mask data to list
-data = []
-for path in get_metadata_paths():
-  with open(path) as file:
-    # Parse to dict and select the 'object' element(s) 
-    metadata = xmltodict.parse(file.read())
-    obj = metadata['annotation']['object']
+def extract_metadata():
+  data = []
+  for path in get_metadata_paths():
+    with open(path) as file:
+      # Parse to dict and select the 'object' element(s) 
+      metadata = xmltodict.parse(file.read())
+      obj = metadata['annotation']['object']
 
-    # If the object is a list loop through it, else select name
-    if type(obj) == list:
-      for o in obj:
-        data.append(o['name'])
-    else:
-      data.append(obj['name'])
+      # If the object is a list loop through it, else select name
+      if type(obj) == list:
+        for o in obj:
+          data.append(o['name'])
+      else:
+        data.append(obj['name'])
+  return data
 
-# Plot mask data
-x = ['With Mask', 'Without Mask', 'Mask Weared Incorrect']
-y = [data.count('with_mask'), data.count('without_mask'), data.count('mask_weared_incorrect')]
+def plot_mask_data():
+  data = extract_metadata()
+  x = ['With Mask', 'Without Mask', 'Mask Weared Incorrect']
+  y = [data.count('with_mask'), data.count('without_mask'), data.count('mask_weared_incorrect')]
 
-fig, ax = plt.subplots()
-ax.bar(x, y)
+  fig, ax = plt.subplots()
+  ax.bar(x, y)
 
-plt.savefig('images/mask_data.png')
+  plt.savefig('images/mask_data.png')
+
+# Plot data and save diagram to file
+plot_mask_data()
+
+# Process images (mark faces)
