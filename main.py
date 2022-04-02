073eb45237c873e2ba6ad1ce4ca5525705439493
Oliver Sabler <oliver.sabler@gmail.com>
2022-04-02 21:16:50 +0200

    finish dataset creation

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import enum
 import xmltodict
+from PIL import Image
+from os import listdir
+
 import numpy as np
 from matplotlib import pyplot as plt
-from os import listdir
+
+import torch
+from torchvision import datasets, transforms, models
 
 WITH_MASK = "with_mask"
 WITHOUT_MASK = "without_mask"
@@ -61,36 +65,66 @@ def plot_mask_data():
 # Plot data and save diagram to file
 # plot_mask_data()
 
-# Process images (mark faces)?
-
 # Dataset
-def handle_obj_list(obj):
-    for i in range(len(obj)):
-        x, y, w, h = list(map(int, obj[i]["bndbox"].values()))
-        label = OPTIONS[obj[i]["name"]]
-
-
-def handle_obj(obj):
-    x, y, w, h = list(map(int, obj["bndbox"].values()))
-    label = OPTIONS[obj["name"]]
-    print(label)
-
-
 def create_dataset():
+    # Get file paths
     images_paths = get_images_paths()
     metadata_paths = get_metadata_paths()
+
+    # Empty tensor lists
     image_tensor = []
     label_tensor = []
 
+    # Composes several transforms together to be used on image
+    t = transforms.Compose([transforms.Resize((226, 226)), transforms.ToTensor()])
+
     data_len = int(len(images_paths)) - 1
     for i in range(data_len):
+        print(f"tensor {i}")
         with open(metadata_paths[i]) as file:
             metadata = xmltodict.parse(file.read())
             obj = metadata["annotation"]["object"]
             if type(obj) == list:
-                handle_obj_list(obj)
+                # Loop through each object (face) in image
+                for i in range(len(obj)):
+                    # map xmin, ymin, xmax and ymax xml properties
+                    x, y, w, h = list(map(int, obj[i]["bndbox"].values()))
+
+                    # Get image label and convert it to OPTION number
+                    label = OPTIONS[obj[i]["name"]]
+
+                    # Crop the image based on xmin, ymin, xmax and ymax (face coordinates)
+                    image = transforms.functional.crop(
+                        Image.open(images_paths[i]).convert("RGB"), y, x, h - y, w - x
+                    )
+
+                    # Apply transforms to image and append image to tensor list
+                    image_tensor.append(t(image))
+
+                    # append label to tensor list
+                    label_tensor.append(torch.tensor(label))
             else:
-                handle_obj(obj)
+                # map xmin, ymin, xmax, ymax xml properties
+                x, y, w, h = list(map(int, obj["bndbox"].values()))
+
+                # Get image label and convert it to OPTION number
+                label = OPTIONS[obj["name"]]
+
+                # Crop the image based on xmin, ymin, xmax and ymax (face coordinates)
+                image = transforms.functional.crop(
+                    Image.open(images_paths[i]).convert("RGB"), y, x, h - y, w - x
+                )
+
+                # Apply transforms to image and append image to tensor list
+                image_tensor.append(t(image))
+
+                # append label to tensor list
+                label_tensor.append(torch.tensor(label))
+
+    # Zip image and label tensor lists
+    dataset = [[j, k] for j, k in zip(image_tensor, label_tensor)]
+    return tuple(dataset)
 
 
-create_dataset()
+ds = create_dataset()
+print(ds[0])
